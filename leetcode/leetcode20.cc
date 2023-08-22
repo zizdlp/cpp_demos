b56a22e62d24cc735d98d201d032ee3905438ad0
zz <zdlp@sina.cn>
2023-08-22 17:47:34 +0800

    update

--- leetcode/leetcode20.cc
+++ leetcode/leetcode20.cc
@@ -7,22 +7,22 @@ class Solution {
 public:
     bool isValid(string s) {
         std::unordered_map<char,char>map={
-               {'}', '{'},
-                {')', '('},
-                {']', '['}
+            {'}', '{'},
+            {')', '('},
+            {']', '['}
         };
         auto n =s.size();
         std::stack<char> st;
         for(auto i =0;i<n;++i){
-           if(map.find(s[i])==map.end()){
-               st.push(s[i]);
-           } else{
-               if(st.empty() || st.top()!=map[s[i]]){
-                   return false;
-               }else{
-                   st.pop();
-               }
-           }
+            if(map.find(s[i])==map.end()){
+                st.push(s[i]);
+            } else{
+                if(st.empty() || st.top()!=map[s[i]]){
+                    return false;
+                }else{
+                    st.pop();
+                }
+            }
         }
         return st.size()==0;
     }
