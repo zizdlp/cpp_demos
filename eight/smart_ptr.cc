5c2c2a9e9171ac66ea48d76fe83d3b6ccfa5bee4
zz <zdlp@sina.cn>
2023-08-25 17:50:42 +0800

    updte

--- eight/smart_ptr.cc
+++ eight/smart_ptr.cc
@@ -1,43 +1,3 @@
-# 八股文
-
-1. explicit 关键字
-
-**explicit** 防止隐式类型转换,典型场景：构造函数。
-
-
-```cpp
-class MyClass {
-public:
-    explicit MyClass(int value) {
-        this->value = value;
-    }
-
-    int getValue() {
-        return value;
-    }
-private:
-    int value;
-};
-
-int main() {
-    MyClass obj1 = 42;  // 这里会导致编译错误，因为构造函数被声明为 explicit
-    MyClass obj2(42);   // 这里是正确的，使用了显式构造函数调用
-    int x = obj2.getValue(); // 正常调用成员函数
-
-    return 0;
-}
-```
-
-
-
-
-
-
-### 智能指针
-
-1. unique_ptr:独占所有权，超过生命周期释放资源。
-
-```cpp
 template <typename T>
 class UniquePtr {
 public:
@@ -85,4 +45,3 @@ public:
 private:
     T* ptr_;
 };
-```
